3711f883ea4ba8ac58faec563e1b9df428bfc806
Nagesh D J <Nagesh D J@TYSS-Nagesh-DJ>
2024-11-12 16:57:21 +0530

    Merge branch 'master' of https://github.com/Nagesh-DJ/com.tyss.frameworkrepo.git

---
+++ src/test/java/pack1/CreateContactTest.java
@@ -0,0 +1,16 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+@Test
+public void createContactTest()
+	{
+	System.out.println("execute createContactTest");
+	}
+@Test
+public void modifyContactTest()
+	{
+	System.out.println("execute modifyContactTest");
+	}
+}
