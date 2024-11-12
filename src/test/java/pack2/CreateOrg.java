ebf9e96318834af533e52ebb59a6d3efc52dc8c2
Nagesh D J <Nagesh D J@TYSS-Nagesh-DJ>
2024-11-12 16:59:05 +0530

    new package added

---
+++ src/test/java/pack2/CreateOrg.java
@@ -0,0 +1,16 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class CreateOrg {
+@Test
+public void createOrgTest()
+	{
+	System.out.println("execute createOrgTest");
+	}
+@Test
+public void modifyOrgTest()
+	{
+	System.out.println("execute modifyOrgTest");
+	}
+}
